9aa183e6ec7a5b840d18249c35849d76eb6a4fa7
itishprasad30 <itishprasad20@gmail.com>
2022-01-08 12:15:47 +0530

    master

--- src/CardDetails.js
+++ src/CardDetails.js
@@ -2,18 +2,32 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Card from "./Card";
 const CardDetails = () => {
+  const [page, setPage] = useState([]);
   const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchPage = async () => {
+      for (let i = 1; i < 11; i++) {
+        const res = await axios.get(
+          `https://api.pokemontcg.io/v2/cards?page=${i}&pageSize=10`
+        );
+        setPage(res.data.page);
+      }
+    };
+    fetchPage();
+  }, []);
 
   useEffect(() => {
     const fetchPosts = async () => {
       const res = await axios.get(
-        "https://api.pokemontcg.io/v2/cards?page=1&pageSize=10"
+        `https://api.pokemontcg.io/v2/cards?page=${page}&pageSize=10`
       );
       setData(res.data.data);
     };
+
     fetchPosts();
-  }, []);
+  }, [page]);
   console.log(data);
+  console.log(page);
   return (
     <div className="card-container">
       {data.map((data, idx) => (
